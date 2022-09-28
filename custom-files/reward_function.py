c1a887c92511d4a799b00aa4c8c0ad48a32d9b91
jperezdelcerro <86670143+jperezdelcerro@users.noreply.github.com>
2022-09-28 15:17:22 -0300

    11

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -1,38 +1,21 @@
 import numpy as np
 import math
 
-action_center = [    
-        {"steering_angle": -10, "speed": 2},        
-        {"steering_angle": -5, "speed": 2.5},
-        {"steering_angle": 0, "speed": 3.0},
-        {"steering_angle": 5,  "speed": 2.5},
-        {"steering_angle": 10, "speed": 2},        
-
-]
-
-action_right = [
+action_space = [
         {"steering_angle": -30, "speed": 1.1},
         {"steering_angle": -25, "speed": 1.2},
         {"steering_angle": -20, "speed": 1.3},
         {"steering_angle": -15, "speed": 1.5},
         {"steering_angle": -10, "speed": 2},        
-]
-
-action_left = [
+        {"steering_angle": -5, "speed": 2.5},
+        {"steering_angle": 0, "speed": 3.0},
+        {"steering_angle": 5,  "speed": 2.5},
         {"steering_angle": 10,   "speed": 2},
         {"steering_angle": 15,   "speed": 1.5},
         {"steering_angle": 20,  "speed": 1.3},
         {"steering_angle": 25,  "speed": 1.2},
         {"steering_angle": 30,  "speed": 1.1}
 ]
-
-action_space = {
-
-    'left' : action_left,
-    'right' : action_right,
-    'center' : action_center,
-
-}
 
 def getTrackDirection(waypoints, closest_waypoints):
     # Calculate the direction of the center line based on the closest waypoints
@@ -55,10 +38,9 @@ def getDirectionDiff(track_direction, heading):
 def getSpeedThereshold(speed):
     return 0.5 if 1.5 >= speed >= 3 else 0.1
 
-def curveSpeedPenalty(direction_diff, speed, reward, direction):  #combinar con lo de dav id, chequear reinforment positivo
+def curveSpeedPenalty(direction_diff, speed, reward):  #combinar con lo de dav id, chequear reinforment positivo
   #if the car isnt going staight, and the speed is 
     threshold = 5
-    action_space = action_space.get(direction)
     for space in action_space:
         if direction_diff == space["steering_angle"] and speed == space['speed']:
             reward += 20
@@ -71,20 +53,6 @@ def curveSpeedPenalty(direction_diff, speed, reward, direction):  #combinar con 
             return reward
     return reward
 
-def curveSpeedPenalty2(direction_diff, speed, reward):  #combinar con lo de dav id, chequear reinforment positivo
-  #if the car isnt going staight, and the speed is 
-    threshold = 5
-    for space in action_space:
-        if direction_diff == space["steering_angle"] and speed == space['speed']:
-            reward += 20
-        elif direction_diff - threshold >= space["steering_angle"] >=  direction_diff + threshold: 
-            threshold = getSpeedThereshold(space['speed'])
-            if space['speed'] - threshold >= speed >= space['speed'] + threshold:
-                reward += 10
-            else:
-                reward -= 10
-            return reward
-    return reward
 
 
 def _getClosestWaypoints(params):
@@ -117,81 +85,7 @@ def reward_function(params):
     track_direction = getTrackDirection(waypoints, closest_waypoints)
     direction_diff = getDirectionDiff(track_direction, heading) 
     
-
-    center_variance = params["distance_from_center"] / params["track_width"]
-
-    speed = params["speed"]
-    closest_waypoints = params["closest_waypoints"]
-    is_left_of_center = params["is_left_of_center"]
-
-
-    left_lane = [14,15,16,17,18,19,20,21,22,23,24,25,26,27,
-                39,40,41,42,43,57,58,59,60,61,62,63,85,86,87,88,89,90,91,  
-                102,103,104,105,106,107,108,109,110,111,112,  
-                133,134,135,136,137,138,139,140,141,142,143,144,145,146,147,148,149,  
-                159,160,161,162,163,164, 194,195,196,197,198,199,200,201,   
-                225,226,227,228,243,244,245,246,247,248,249,250 ]
-
-    center_lane = [1,2,3,4,5,6,7,8,9,10,11,12,13,28,29,30,31,36,37,38,  
-                   44,45,46,52,53,54, 55,56,64,65,66,67,68,69,70,79,80,81, 82,83,84,  92,93,   
-                   99,100,101, 112,113,114,  
-                   114,115,116,117,118,119,120,121,122,123,124,125,126,127,128,129, 
-                   130,131,132, 144,145,146,147,148,149, 
-                   150,151,152,153,154,  157,158,  165,166,167,168, 
-                   176,177,178,179,180,181,182,183,184,185,186, 
-                   191,192,193, 202,203, 210,211,212,213,214,215,216,218,219,220,221,222,223,224,225,226,227,228,229,230, 
-                   240,241,242,  250,251,252,253,254]
-
-
-    right_lane = [32,33,34,35,36, 
-                  47,48,49,50,51,
-                  71,72,73,74,75,76,77,78,
-                  93,94,95,96,97,98,99,100,  
-                  154,155,156,   168,169,170,171,172,173,174,175,  
-                  187,188,189,190,  204,205,206,207,208,209,210,211,212, 
-                  231,232,233,234,235,236,237,238,239
-                                     ]
-
-    fast = [1,2,3,4,5,6,7,8,9,10,11,12,   
-            27,28,29,30,31,32,33,34,35, 
-            46,47,48,49,
-            81,82,83,84,85,86,87,88,
-            114,115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,131,132,133,134,135,136,137,138,139,140,141,142,143,
-            155,156,157,158,  
-            164,165,166,167,168,169,170,171,172,173,174,175,176,177,178,179,180,181,182,183,184,  
-            202,203,204,205,206,207,208,209,210,211,212,213,214,214,215,216,217,218,219,220,221,222,223,224,225,226, 
-            242,243,244,245,246,247,248,249,250,251,252,253,254        ] 
-
-
-
-    slow = [50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,  
-            70,71,72,73,74,75,76,77,78,79,80, 89,90,91,92,93,94,95,96,  
-            103,104,105,106,107,108,109,110,111,112,  191,192,193,194,195,196,197,198,199,200,201,202, 
-            227,228,229,230,231,232,233,234,235,236,237,238,239,240,241
-                    ] 
-
-    middle = [13,14,15,16,17,18,19,20,21,22,24,25,26,
-              65,66,67,
-              97,98,99,100,101,102, 113,114 ]
-
-
-    middle1 = [36,37,38,39,40,41,42,43,44,45,
-               144,145,146,147,148,149,150,151,152,153,154,155,
-               159,160,161,162,163,164, 185,186,187,188, 189,190,191]
-
-
     reward = 21
-
-    if closest_waypoints[1] in left_lane and is_left_of_center:
-        reward = curveSpeedPenalty(direction_diff, speed, reward, 'right')
-    elif closest_waypoints[1] in right_lane and not is_left_of_center:
-        reward = curveSpeedPenalty(direction_diff, speed, reward, 'left')
-    elif closest_waypoints[1] in center_lane and center_variance < 0.4:
-        reward = curveSpeedPenalty(direction_diff, speed, reward, 'center')
-    else:
-        reward -= 20
-
-
-    return float(reward)
+    reward = curveSpeedPenalty(direction_diff, speed, reward)
     
     return float(reward)
